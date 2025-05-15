43200297af7acaf587f3c703e04d6f2236ac30c7
Luka Zukhbaia <145780979+lukazu25@users.noreply.github.com>
2025-05-15 14:11:08 +0400

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #include <X11/XF86keysym.h>
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
