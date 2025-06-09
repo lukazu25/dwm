9775b4150dc027c4fe0a39e074917a2908486a38
Luka Zukhbaia <145780979+lukazu25@users.noreply.github.com>
2025-06-09 12:55:31 +0400

    Update gruvbox.h

--- dwm/gruvbox.h
+++ dwm/gruvbox.h
@@ -4,3 +4,11 @@ static const char col_gray3[]   = "#ebdbb2";  // light1 - main foreground (text)
 static const char col_gray4[]   = "#3c3836";  // dark2 - subtle contrast for borders or highlights
 static const char col_accent[]  = "#fe8019";  // bright orange - accent color
 
+
+static const char *colors[][3]      = {
+
+/*               fg         bg         border   */
+        [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+        [SchemeSel]  = { col_gray4, col_accent,  col_accent  },
+        [SchemeHid]  = { col_accent,  col_gray1, col_accent  },
+};
