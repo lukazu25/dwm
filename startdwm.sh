13ded3a7fa2bf3d3877f102c02408ef4dce73745
Luka Zukhbaia <145780979+lukazu25@users.noreply.github.com>
2025-06-21 22:12:48 +0400

    Update startdwm.sh

--- startdwm.sh
+++ startdwm.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 xrandr --output eDP --mode 1920x1080
-setxkbmap -layout us,ru,ge -option 'grp:alt_space_toggle'
+setxkbmap -layout us,ru,ge -option 'grp:alt_shift_toggle'
 nitrogen --restore &
 slstatus &
+# Set Cursor
+# export XCURSOR_SIZE=24
+# export XCURSOR_THEME="Your Cursor"
 
 
 while true; do
