774c1edd7c9ea3800451c0861d1e342571bdf92a
Luka Zukhbaia <145780979+lukazu25@users.noreply.github.com>
2025-07-18 17:58:56 +0400

    Update solarized.h

--- dwm/solarized.h
+++ dwm/solarized.h
@@ -3,6 +3,7 @@ static const char col_gray2[]       = "#073642";
 static const char col_gray3[]       = "#839496";
 static const char col_gray4[]       = "#93a1a1";
 static const char col_accent[]        = "#174956";
+
 static const char *colors[][3]      = {
     /*               fg         bg         border   */
     [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
