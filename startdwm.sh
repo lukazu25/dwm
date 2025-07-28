39bb4dcc1901255233891237816e9431883207a5
Luka Zukhbaia <145780979+lukazu25@users.noreply.github.com>
2025-07-28 14:54:55 +0400

    Update startdwm.sh

--- startdwm.sh
+++ startdwm.sh
@@ -5,7 +5,7 @@ setxkbmap -layout us,ru,ge -option 'grp:alt_shift_toggle'
 nitrogen --restore &
 slstatus &
 # For Cursor Use ~/.Xresources and xrdb -merge ~/.Xresources For Qt Apps qt5ct or qt6ct 
-
+# In order for blueman-applet to work, start it after D-Bus and Polkit
 
 while true; do
 	# Log stderror to a file
